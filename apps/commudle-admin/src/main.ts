1d4985e927753d79d90485722b54ecb936914110
Arshdeep singh <arsh199820@gmail.com>
2023-10-22 20:39:38 +0530

    feat: integrate sentry

--- apps/commudle-admin/src/main.ts
+++ apps/commudle-admin/src/main.ts
@@ -4,8 +4,36 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as Sentry from '@sentry/angular-ivy';
+
 if (environment.production) {
   enableProdMode();
+  Sentry.init({
+    dsn: environment.sentry_dsn,
+    integrations: [
+      // Registers and configures the Tracing integration,
+      // which automatically instruments your application to monitor its
+      // performance, including custom Angular routing instrumentation
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.routingInstrumentation,
+        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ['localhost', environment.base_url],
+      }),
+      // Registers the Replay integration,
+      // which automatically captures Session Replays
+      new Sentry.Replay(),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
 }
 
 platformBrowserDynamic()
